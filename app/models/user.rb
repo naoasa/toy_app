75e414ab00d4dea55d8528b8cdfb00ff59953578
naoasa <asaumi1106n@gmail.com>
2023-08-10 01:15:17 +0900

    Next 2.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :microposts
-    # name, emailともに空値を許さない
+    # name, emailともに空値を許さない(バリデーション)
     validates :name, presence: true
     validates :email, presence: true
 end
